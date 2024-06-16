7af1f2ec25cbcc1067631956ea92fb8a1b2d6507
Сырцев Вадим Игоревич <syrtcevvi@gmail.com>
2024-06-16 21:43:31 +0300

    Initial commit

---
+++ examples/esp-hal/src/main.rs
@@ -0,0 +1,23 @@
+#![no_std]
+#![no_main]
+
+use esp_backtrace as _;
+use esp_hal::{
+    clock::ClockControl, delay::Delay, peripherals::Peripherals, prelude::*, system::SystemControl,
+};
+
+#[entry]
+fn main() -> ! {
+    let peripherals = Peripherals::take();
+    let system = SystemControl::new(peripherals.SYSTEM);
+
+    let clocks = ClockControl::max(system.clock_control).freeze();
+    let delay = Delay::new(&clocks);
+
+    esp_println::logger::init_logger_from_env();
+
+    loop {
+        log::info!("Hello world!");
+        delay.delay(500.millis());
+    }
+}
